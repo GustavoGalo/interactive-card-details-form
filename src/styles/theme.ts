78da64b95c11d12be0d2bd6542edb5e18abd0dbd
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-09-03 17:36:33 -0300

    style: ensure theme concerns style guide

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,18 @@
 export const theme = {
+  breakingPoints: {
+    mobile: 375,
+    desktop: 1440,
+  },
   colors: {
-    primary: '#000',
-    font: 'white',
+    red: 'hsl(0, 100%, 66%)',
+    white: 'hsl(0, 0%, 100%)',
+    lightGrayishViolet: 'hsl(270, 3%, 87%)',
+    darkGrayishViolet: 'hsl(279, 6%, 55%)',
+    veryDarkViolet: 'hsl(278, 68%, 11%)',
+  },
+  linearGradient: {
+    from: 'hsl(249, 99%, 64%)',
+    to: 'hsl(278, 94%, 30%)',
   },
 };
 
