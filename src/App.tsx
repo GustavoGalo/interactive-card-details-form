5fd3808b4917b59ae20e3b13dfcda15a29027fdf
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-09-03 16:34:41 -0300

    chore: remove css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import './App.css';
-
-import Logo from './logo.svg';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={Logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
