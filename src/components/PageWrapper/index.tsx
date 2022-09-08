a47d20488cd24e24af8bec7581c3eb548e7b1ee7
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-09-07 21:41:29 -0300

    feat: responsive PageWrapper

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from 'react';
 
-import { Background } from './styles';
 import bgDesktop from '../../images/bg-main-desktop.png';
 import bgMobile from '../../images/bg-main-mobile.png';
+
+import { Background, CenteredContainer, Container } from './styles';
 
 interface Props {
   children?: ReactNode;
 }
 
 export const PageWrapper = ({ children }: Props) => (
-  <Background src={bgDesktop} mobileSrc={bgMobile}>
-    {children}
-  </Background>
+  <Container>
+    <Background src={bgDesktop} mobileSrc={bgMobile}>
+      <CenteredContainer>{children}</CenteredContainer>
+    </Background>
+  </Container>
 );
--- src/components/PageWrapper/styles.ts
+++ src/components/PageWrapper/styles.ts
@@ -5,10 +5,27 @@ interface IBackgroundProps {
   mobileSrc: string;
 }
 
-export const Background = styled.div<IBackgroundProps>`
+export const Container = styled.main`
+  height: 100vh;
   width: 100%;
+`;
+
+export const Background = styled.div<IBackgroundProps>`
+  width: 33%;
   height: 100vh;
   background-image: url(${(props) => props.src});
   background-repeat: no-repeat;
-  background-size: contain;
+  background-size: cover;
+
+  @media (max-width: ${(props) => props.theme.breakingPoints.smallDesktop}px) {
+    background-image: url(${(props) => props.mobileSrc});
+    width: 100%;
+    height: 240px;
+  }
+`;
+
+export const CenteredContainer = styled.div`
+  position: absolute;
+  offset: 0;
+  width: 100%;
 `;
