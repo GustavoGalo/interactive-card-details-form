0333182573227539e2612674782a53cec28e7219
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-09-18 00:35:23 -0300

    refactor: wrap ThemProvider and react-redux Provider into PageWrapper component

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,7 +1,12 @@
 import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { store } from '../../App/store';
 
 import bgDesktop from '../../images/bg-main-desktop.png';
 import bgMobile from '../../images/bg-main-mobile.png';
+import { GlobalStyles } from '../../styles/global';
+import { theme } from '../../styles/theme';
 
 import { Background, CenteredContainer, Container } from './styles';
 
@@ -10,9 +15,14 @@ interface Props {
 }
 
 export const PageWrapper = ({ children }: Props) => (
-  <Container>
-    <Background src={bgDesktop} mobileSrc={bgMobile}>
-      <CenteredContainer>{children}</CenteredContainer>
-    </Background>
-  </Container>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Background src={bgDesktop} mobileSrc={bgMobile}>
+          <CenteredContainer>{children}</CenteredContainer>
+        </Background>
+        <GlobalStyles />
+      </Container>
+    </ThemeProvider>
+  </Provider>
 );
